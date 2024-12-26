32761e8cc7ad7ecdf0c3e60e42a2a379e610c5a3
busraylzm <busra.123456k@gmail.com>
2024-12-26 18:59:17 +0300

    bilgi güvenliği
     kullanıcıdan aldığı anahtarlar ile sezar  şifrelemeyi hem ascıı tablosuna göre hemde alfabeye göre yapıp afini ise sadece alfabeye göre yapmaktadır.

---
+++ BİLGİ GÜVENLİĞİ/uygulama/uygulama/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace uygulama
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label1.Location = new System.Drawing.Point(85, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "METİN";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(109, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 25);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "ASCİİ ŞİFRELE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label2.Location = new System.Drawing.Point(58, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "ANAHTAR 1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(109, 287);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 25);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "SEZAR ŞİFRELE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(258, 211);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(84, 25);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "HESAPLA";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(132, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(90, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(132, 105);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(90, 20);
+            this.textBox2.TabIndex = 15;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(109, 252);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(113, 20);
+            this.textBox3.TabIndex = 16;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(109, 327);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(113, 20);
+            this.textBox4.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label3.Location = new System.Drawing.Point(19, 259);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "ŞİFRELİ METİN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label4.Location = new System.Drawing.Point(19, 334);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "ŞİFRELİ METİN";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(258, 252);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(369, 173);
+            this.listBox1.TabIndex = 20;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(109, 366);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(113, 25);
+            this.button4.TabIndex = 21;
+            this.button4.Text = "AFİN ŞİFRELE";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(109, 408);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(113, 20);
+            this.textBox5.TabIndex = 22;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 415);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 13);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "ŞİFRELİ METİN";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label6.Location = new System.Drawing.Point(58, 143);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 13);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "ANAHTAR 2";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(132, 136);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(90, 20);
+            this.textBox6.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.BackgroundImage = global::uygulama.Properties.Resources.ŞİFRELEME_PROGRAMI;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(833, 541);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ BİLGİ GÜVENLİĞİ/uygulama/uygulama/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // textBox1'den metni al
+            string metin = textBox1.Text;
+
+            // anahtar numarası gir
+            int anahtar;
+
+            if (int.TryParse(textBox2.Text, out anahtar))
+            {
+                string sifrelimetin = Sifrele(metin, anahtar);
+                textBox3.Text = sifrelimetin;
+            }
+            else
+            {
+                MessageBox.Show("Geçerli bir anahtar girin!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            // Şifrelenmiş metni hesapla
+            string sifrelenmisMetin = Sifrele(metin, anahtar);
+
+            // Şifrelenmiş metni textBox3'ye yaz
+            textBox3.Text = sifrelenmisMetin;
+        }
+
+        // Tüm karakterler için Caesar şifreleme fonksiyonu
+        private string Sifrele(string metin, int anahtar)
+        {
+            char[] karakterler = metin.ToCharArray();
+            for (int i = 0; i < karakterler.Length; i++)
+            {
+                // Her karakteri ASCII tablosuna göre kaydır
+                karakterler[i] = (char)((karakterler[i] + anahtar) % 256);
+            }
+            return new string(karakterler);
+        }
+
+        // ASCII kodlarını metin olarak döndüren fonksiyon
+        private string GetAsciiKodlari(string metin)
+        {
+            StringBuilder asciiKodlari = new StringBuilder();
+            foreach (char karakter in metin)
+            {
+                asciiKodlari.Append($"{karakter}: {(int)karakter}, ");
+            }
+            return asciiKodlari.ToString().TrimEnd(',', ' ');
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // textBox1'den metni al
+            string metin = textBox1.Text;
+
+            // ListBox'u temizle (önceki sonuçları sil)
+            listBox1.Items.Clear();
+
+            // ASCII bilgilerini listeye ekle
+            foreach (var satir in GetKodlamaListe(metin))
+            {
+                listBox1.Items.Add(satir);
+            }
+        }
+        private List<string> GetKodlamaListe(string metin)
+        {
+            List<string> liste = new List<string>();
+
+            foreach (char karakter in metin)
+            {
+                int asciiKod = (int)karakter;
+                string binaryKod = Convert.ToString(asciiKod, 2).PadLeft(8, '0'); // Binary (8 bit)
+                string octalKod = Convert.ToString(asciiKod, 8);                 // Octal
+                string hexKod = Convert.ToString(asciiKod, 16).ToUpper();        // Hexadecimal
+
+                // Formatlı şekilde bilgileri listeye ekle
+                string satir = $"Karakter: '{karakter}', ASCII: {asciiKod}, Binary: {binaryKod}, Octal: {octalKod}, Hex: {hexKod}";
+                liste.Add(satir);
+            }
+
+            return liste;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // textBox1'den metni al
+            string metin = textBox1.Text;
+
+            // anahtar numarası gir
+            int anahtar;
+
+            if (int.TryParse(textBox2.Text, out anahtar))
+            {
+                string sifrelimetin = Sifrele(metin, anahtar);
+                textBox4.Text = sifrelimetin;
+            }
+            else
+            {
+                MessageBox.Show("Geçerli bir anahtar girin!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            // Şifrelenmiş metni hesapla (sadece alfabe karakterlerine göre)
+            string sifrelenmisMetin = SadeceAlfabeSifrele(metin, anahtar);
+
+            // Şifrelenmiş metni textBox2'ye yaz
+            textBox4.Text = sifrelenmisMetin;
+        }
+
+        // Şifreleme fonksiyonu
+        private string SadeceAlfabeSifrele(string metin, int anahtar)
+        {
+            char[] karakterler = metin.ToCharArray();
+            for (int i = 0; i < karakterler.Length; i++)
+            {
+                char karakter = karakterler[i];
+                if (char.IsLetter(karakter))
+                {
+                    char offset = char.IsUpper(karakter) ? 'A' : 'a';
+                    karakter = (char)(((karakter + anahtar - offset) % 26) + offset);
+                }
+                karakterler[i] = karakter;
+            }
+            return new string(karakterler);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // textBox1'den şifrelenecek metni al
+            string metin = textBox1.Text;
+
+            // textBox2 ve textBox6 anahtarları al
+            if (!int.TryParse(textBox2.Text, out int a) || !int.TryParse(textBox6.Text, out int b))
+            {
+                MessageBox.Show("Lütfen geçerli bir tam sayı giriniz!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // 'a' anahtarının geçerli olup olmadığını kontrol et
+            if (GCD(a, 26) != 1)
+            {
+                MessageBox.Show("'a' anahtarı 26 ile aralarında asal olmalıdır!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Şifrelenmiş metni hesapla
+            string sifrelenmisMetin = AffineSifrele(metin, a, b);
+
+            // Şifrelenmiş metni textBox2'ye yaz
+            textBox5.Text = sifrelenmisMetin;
+        }
+        private int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+        private string AffineSifrele(string metin, int a, int b)
+        {
+            char[] karakterler = metin.ToCharArray();
+
+            for (int i = 0; i < karakterler.Length; i++)
+            {
+                char karakter = karakterler[i];
+                if (char.IsLetter(karakter))
+                {
+                    char offset = char.IsUpper(karakter) ? 'A' : 'a'; // Büyük/küçük harf kontrolü
+                    int x = karakter - offset; // Alfabetik sıra numarasını al
+                    int y = (a * x + b) % 26;  // Affine şifreleme formülü
+                    karakterler[i] = (char)(y + offset); // Şifrelenmiş karakteri hesapla
+                }
+            }
+
+            return new string(karakterler);
+        }
+    }
+}
